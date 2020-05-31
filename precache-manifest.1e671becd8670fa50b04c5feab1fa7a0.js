436c924f02d7263a07ea036cc7f8aec6c84e0d97
united.against <dummy@dummy.com>
2020-05-31 18:40:34 +0200

    Updates

---
+++ precache-manifest.1e671becd8670fa50b04c5feab1fa7a0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c24301fb22d2b37cc99ecc16a743107f",
+    "url": "/calendar/index.html"
+  },
+  {
+    "revision": "2fbe5cdacf20e72c8fad",
+    "url": "/calendar/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "cc6f198a6c95c2ecfbfa",
+    "url": "/calendar/static/js/2.c9173091.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/calendar/static/js/2.c9173091.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2fbe5cdacf20e72c8fad",
+    "url": "/calendar/static/js/main.403803c4.chunk.js"
+  },
+  {
+    "revision": "d237a9a33f29fd2115a3",
+    "url": "/calendar/static/js/runtime-main.a47d4abe.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/calendar/static/media/logo.5d5d9eef.svg"
+  }
+]);
